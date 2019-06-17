7d3a12150bc7cfb71f534257f769a83e342b21b7
slomkarafa <slomkarafa@gmail.com>
2019-06-17 07:24:13 +0200

    rpycinit.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 source activate rpi-tracker
 
+trap 'kill %1' SIGINT
+
 PYTHONPATH=$PYTHONPATH:../ python communication/service.py &
 sleep 2
 PYTHONPATH=$PYTHONPATH:../ python slam/service.py
