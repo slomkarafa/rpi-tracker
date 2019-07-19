9713d1de3121b0bb034800e5dde73c174fc76fa6
slomkarafa <slomkarafa@gmail.com>
2019-07-19 08:06:06 +0200

    configs

--- install.sh
+++ install.sh
@@ -15,4 +15,32 @@ sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
+sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake ninja-build
+
+
+mkdir -p ros/ && cd ros/
+sudo rosdep init
+rosdep update
+rosinstall_generator ros_comm sensor_msgs --rosdistro melodic --deps --wet-only --tar > melodic-ros_comm-wet.rosinstall
+wstool init src melodic-ros_comm-wet.rosinstall
+rosdep install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
+
+sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
+workon tracker
+pip install tornado==4.5.3
+mv src ros_src
+mkdir src
+cd src
+catkin_init_workspace
+
+git clone https://github.com/RobotWebTools/rosbridge_suite.git
+git clone https://github.com/robopeak/rplidar_ros.git
+git clone https://github.com/googlecartographer/cartographer_ros.git
+
+catkin_make_isolated --install --use-ninja --source external_src
+Base path: /home/pi/rpi-tracker/ros
+Source space: /home/pi/rpi-tracker/ros/src
